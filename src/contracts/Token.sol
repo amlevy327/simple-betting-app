e1cb41d0c5c0209fd6a2dea2593db972650f5dec
Andrew <amlevy327@gmail.com>
2022-01-30 14:37:54 -0600

    misc updates, deploy on rinkeby

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -8,5 +8,9 @@ contract Token is Ownable, ERC20 {
 
   constructor (string memory _name, string memory _symbol) ERC20 (_name, _symbol) {
     _mint(msg.sender, 1000000 * (10 ** 18));
+    _mint(0x1460c2f091Cd351DaB0C01712a819AC3c253822D, 1000000 * (10 ** 18));
+    _mint(0x1460c2f091Cd351DaB0C01712a819AC3c253822D, 1000000 * (10 ** 18));
+    _mint(0xa04a095A5114dFAc9A08fa18ce894676E2dB5190, 1000000 * (10 ** 18));
+    _mint(0xAD0F862090850Ac7D40d27d847f0B0af8daba52B, 1000000 * (10 ** 18));
   }
 }
